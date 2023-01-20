219071688dbd25aa932565cd4e8e2d1f862f3d07
naumaan <113992053+naumaan12@users.noreply.github.com>
2023-01-20 15:05:09 +0530

    mobileNumber Int

--- controllers/users.go
+++ controllers/users.go
@@ -10,7 +10,7 @@ import (
 type CreateUserInput struct {
 	Name         string `json:"name" binding:"required"`
 	Email        string `json:"email" binding:"required"`
-	MobileNumber string `json:"mobileNumber" binding:"required"`
+	MobileNumber int    `json:"mobileNumber" binding:"required"`
 	Age          int    `json:"age" binding:"required"`
 	Password     string `json:"password" binding:"required"`
 }
